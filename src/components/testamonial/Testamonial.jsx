59de4fe8f0a34c4f7a7ab0d4b79d869ad6ed0337
Imalsha Ranepura <61657083+Imalsha97@users.noreply.github.com>
2022-03-03 21:48:19 -0800

    about/home

---
+++ src/components/testamonial/Testamonial.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './testamonial.css';
+
+const Testamonial = () => {
+  return (
+    <section id ="testamonial">Testamonial</section>
+  )
+}
+
+export default Testamonial
